f03c6335824105e024392cb64ab49558fe7ee3cb
Vishal_pathak <vishal.pathak@quantiphi.com>
2020-09-18 22:37:20 +0530

    string hash

--- python/hash_starter.py
+++ python/hash_starter.py
@@ -3,12 +3,12 @@ from linked_list import *
 
 class HSET:
     '''hash sets'''
-    def __init__(self, m=10, big_prime=9999991):
+    def __init__(self, m=20, big_prime=1000013):
         self.store = [LinkedList() for i in range(m)]
         self.m = m
         self.bp = big_prime
 
-    def ah(self, number, a=10, b=5):
+    def ah(self, number, a=34, b=2):
         return ((a*number + b) % self.bp) % self.m
 
     def is_in(self, number):
@@ -27,9 +27,20 @@ class HSET:
             hash_ = self.ah(number)
             self.store[hash_].remove(number)
 
+class StringHash(HSET):
+    def ah(self, string='', a=31,):
+        h_ = 0
+        for i, s in enumerate(string):
+            h_ += ord(s)*(a**i) % self.bp
+        return h_ % self.m
+
+
 
 if __name__ == '__main__':
     import random
+
+
+    c = """
     s = random.choices(range(900), k=20)
 
     print(s)
@@ -57,10 +68,32 @@ if __name__ == '__main__':
     for i in to_remove:
         print(f'is_in {i}', set_.is_in(i))
 
-    print([i.len for i in set_.store])
-    
+    print([i.len for i in set_.store])"""
+
+    s_to_hash = [
+        "Ravi",
+        "Raman",
+        "Harish",
+        "vishal",
+        "Vishal",
+    ]
+
+    print(s_to_hash)
+
+    s_hash = StringHash()
+    s_hash.add("Raman")
+    s_hash.add("Harish")
+    print('False:', s_hash.is_in("Raghav"))
+    print('True:', s_hash.is_in("Harish"))
+
+    s_hash.add('Vishal')
+    print('False:', s_hash.is_in('vishal'))
+    # s_hash.remove('Vishal')
+    s_hash.remove('vishal')
+    print('False:', s_hash.is_in('Vishal'))
+    print(s_hash.store)
 
 
-            
+
 
 
